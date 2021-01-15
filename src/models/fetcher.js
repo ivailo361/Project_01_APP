5b34ee9e0c87eab1fd1177023dde13db89779dad
ivailo361 <ivailo361@gmail.com>
2021-01-14 23:40:02 -0800

    kojk

--- src/models/fetcher.js
+++ src/models/fetcher.js
@@ -12,7 +12,7 @@ const env = process.env.NODE_ENV;
 // const config = require('./config')[env];
 
 // import msg from './messages'
-const BASE_URL = config[env].URL
+const BASE_URL = "https://also-express-srv.herokuapp.com"
 
 
 console.log(env)
